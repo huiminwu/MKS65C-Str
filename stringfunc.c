fcdbaf3312d41d1c88a8d7da35b7c3a191461113
wzhou2 <wzhou2@stuy.edu>
2018-09-28 15:14:51 -0400

    initial func written tbt

--- stringfunc.c
+++ stringfunc.c
@@ -3,7 +3,27 @@
 #include "stringfunc.h"
 
 int mystrlen(char *s) {
-  int size = 0;
-  int x;
-  for ( x = 0; *(s+x) != '\0'; x++)
+  int size;
+  for (size = 0; *(s+size) != '\0'; size++);
+  return size;
 }
+
+char * mystrncpy(char *dest, char *source, int n) {
+  int i;
+  for (i = 0; i < n && *(dest+i) != '\0'; i++) {
+    *(dest+i) = *(source+i);
+  }
+  return dest;
+}
+
+char * strncat(char *dest, char *source, int n) {
+  char *original = dest;
+  while(*(dest++) != '\0');
+  int i;
+  for (i = 0; i < n && *(dest+i) != '\0'; i++) {
+    *(dest+i) = *(source+1);
+  }
+  *dest = '\0';
+  return original
+}
+
