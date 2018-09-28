a004fe89fb56beb26554364266d16dc73c7e1432
wzhou2 <wzhou2@stuy.edu>
2018-09-28 15:34:36 -0400

    init test @strncpy now

--- stringfunc.c
+++ stringfunc.c
@@ -16,7 +16,7 @@ char * mystrncpy(char *dest, char *source, int n) {
   return dest;
 }
 
-char * strncat(char *dest, char *source, int n) {
+char * mystrncat(char *dest, char *source, int n) {
   char *original = dest;
   while(*(dest++) != '\0');
   int i;
@@ -24,6 +24,37 @@ char * strncat(char *dest, char *source, int n) {
     *(dest+i) = *(source+1);
   }
   *dest = '\0';
-  return original
+  return original;
 }
 
+int main() {
+  char s1[10] ="HELLO!";
+  char s2[] = "hello?";
+  char *s3 = "hello";
+  char s4[] = "";
+
+  printf("S! %s\n", s1);
+  printf("S2 %s\n", s2);
+  printf("S3 %s\n", s3);
+  printf("S4 %s\n", s4);
+  
+  
+  printf("LEN TESTING\n");
+
+  printf("standard %ld\n", strlen(s1));
+  printf("homemade %d\n", mystrlen(s1));
+
+  printf("standard %ld\n", strlen(s3));
+  printf("homemade %d\n", mystrlen(s3));
+
+  printf("standard %ld\n", strlen(s4));
+  printf("homemade %d\n", mystrlen(s4));
+
+  printf("Testing strncpy (s1, s3, 3)\n");
+  printf("standard %s\n", strncpy(s1, s3, 6));
+  printf("homemade %s\n", strncpy(s1, s3, 6));
+
+  
+  
+  return 0;
+}
